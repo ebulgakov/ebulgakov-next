8e6d63d0ac5e20d9051787ac2b4d99d699b23354
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-21 10:43:45 +0400

    Add base intro section

--- app/(home)/home-wall.tsx
+++ app/(home)/home-wall.tsx
@@ -2,25 +2,31 @@
 
 import Image from "next/image";
 import { useTranslations } from "use-intl";
+import { Mouse, ChevronDown } from "lucide-react";
 
 function HomeWall() {
   const t = useTranslations("HomePage");
   return (
-    <div className="relative h-screen w-full overflow-hidden flex justify-center items-center z-0">
+    <div className="relative z-0 flex h-[70vh] w-full items-center justify-center overflow-hidden  shadow-lg">
       <Image
-        className="pointer-events-none absolute top-0 left-0 h-full w-full object-cover -z-1"
+        className="pointer-events-none absolute top-0 left-0 -z-1 h-full w-full object-cover"
         src="/img/intro_bg.jpg"
         alt="Intro Background"
         width={1920}
         height={1080}
         priority
       />
-      <div className="text-white text-center p-4 bg-black/50 rounded space-y-3">
+      <div className="space-y-3 rounded bg-black/50 p-4 text-center text-white">
         <div className="text-5xl font-bold">{t("introTitle")}</div>
         <div className="text-2xl">{t("introText")}</div>
       </div>
 
-
+      <div className="absolute invert bottom-0 left-0 flex w-full flex-col items-center -space-y-4 pb-10 text-white ">
+        <Mouse className="mb-0 size-8 rotate-180" />
+        <ChevronDown className="animate-pulse duration-300" />
+        <ChevronDown className="animate-pulse delay-150 duration-300" />
+        <ChevronDown className="animate-pulse delay-300 duration-300" />
+      </div>
     </div>
   );
 }
