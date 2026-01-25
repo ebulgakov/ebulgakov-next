f65921710ae8cc2475a9e01150dc746f45aa6920
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-26 00:59:27 +0400

    Center login section

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,11 +1,13 @@
 import { AuthView } from "@neondatabase/auth/react";
 
+import { cn } from "@/app/lib/utils";
+
 import styles from "./auth-page.module.css";
 
 async function AuthPage({ params }: { params: Promise<{ path: string }> }) {
   const { path } = await params;
   return (
-    <main className={styles.auth}>
+    <main className={cn(styles.auth, "flex min-h-[100%] flex-col items-center justify-center")}>
       <AuthView className="mx-auto" path={path} />
     </main>
   );
