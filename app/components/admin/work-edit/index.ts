70d058ce16c6922529126abaee31f2d8f35400dd
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-25 10:57:36 +0400

    Remove unused files

--- app/components/admin/work-edit/index.ts
+++ app/components/admin/work-edit/index.ts
@@ -1,5 +1,4 @@
 export { WorkEdit } from "./work-edit";
 export { UploadImagePreview } from "./upload-image-preview";
-export { UploadImageNew } from "./upload-image-new";
 export { UploadImagePlaceholder } from "./upload-image-placeholder";
 export { UpdateMedia } from "./update-media";
