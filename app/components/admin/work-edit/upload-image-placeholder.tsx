ea05784adf708ad9a016d3d9375230a42b69cf13
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-25 11:16:46 +0400

    Upload image to work

--- app/components/admin/work-edit/upload-image-placeholder.tsx
+++ app/components/admin/work-edit/upload-image-placeholder.tsx
@@ -1,42 +1,44 @@
 "use client";
 
+import { CldUploadWidget } from "next-cloudinary";
+
 import type { WorkImage } from "@/types/image";
-import type { ChangeEvent } from "react";
 
 type UploadImagePlaceholderProps = {
   onAdd: (image: WorkImage) => void;
 };
 
 function UploadImagePlaceholder({ onAdd }: UploadImagePlaceholderProps) {
-  const handleAddClick = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!event?.target?.files) return;
-
-    const reader = new FileReader();
-    const [file] = event.target.files;
-
-    reader.addEventListener("load", () => {
-      // TODO: Upload the image to Cloudinary and get the public_id
-      // onAdd?.({
-      //   preview: reader.result as string,
-      //   source: file
-      // });
-    });
-
-    reader.readAsDataURL(file);
-  };
-
   return (
-    <div className="w-50">
-      <label className="flex size-50 cursor-pointer items-center justify-center rounded border-2 border-dashed border-gray-300 transition hover:border-gray-800">
-        <span className="text-gray-500">Add Image</span>
-        <input
-          className="invisible absolute"
-          type="file"
-          accept="image/jpeg,image/png,image/gif"
-          onChange={handleAddClick}
-        />
-      </label>
-    </div>
+    <CldUploadWidget
+      uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}
+      signatureEndpoint="/api/sign-cloudinary-params"
+      onSuccess={result => {
+        if (typeof result.info === "object" && "secure_url" in result.info) {
+          onAdd({
+            public_id: result.info.public_id,
+            preview: result.info.secure_url
+          });
+        }
+      }}
+      onError={error => {
+        console.error("Cloudinary Upload Error:", error);
+      }}
+      options={{
+        singleUploadAutoClose: true
+      }}
+    >
+      {({ open }) => {
+        return (
+          <button
+            onClick={() => open()}
+            className="flex size-50 cursor-pointer items-center justify-center rounded border-2 border-dashed border-gray-300 transition hover:border-gray-800"
+          >
+            <span className="text-gray-500">Add Image</span>
+          </button>
+        );
+      }}
+    </CldUploadWidget>
   );
 }
 
