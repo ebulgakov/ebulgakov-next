8afdc4aae53de9a9ff169c23895d017db348047c
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-25 19:04:37 +0400

    Add footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Container } from "@/app/components/ui/container";
+
 function Footer() {
-  return <div>Stub!</div>;
+  return (
+    <footer className="mt-12 bg-gray-900 py-6 text-white">
+      <Container>
+        <div className="flex items-center gap-4">
+          <Link href="/">
+            <Image width={33} height={30} src="/img/logo.svg" alt="Ebulgakov Logo" />
+          </Link>
+
+          <div>
+            Made with love &copy; 2026
+          </div>
+        </div>
+      </Container>
+    </footer>
+  );
 }
 
 export { Footer };
